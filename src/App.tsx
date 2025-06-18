c0f85a47df26e5d59cf86075c33860370c5fa907
Otavio Chaves <otaviocs2005@gmail.com>
2025-06-18 20:18:50 -0300

    data.ts

--- src/App.tsx
+++ src/App.tsx
@@ -2,67 +2,9 @@ import Book from "./components/book";
 import Footer from "./components/footer";
 import Header from "./components/header"
 import Title from "./components/title";
+import { booksList } from "./data";
 import "./App.css"
 
-
-const booksList = [
-  {
-    id: 1,
-    title: "O Senhor dos Anéis",
-    pages: 1178,
-    isRead: true,
-    isFavorite: true
-  },
-  {
-    id: 2,
-    title: "1984",
-    pages: 328,
-    isRead: false,
-    isFavorite: false
-  },
-  {
-    id: 3,
-    title: "Dom Casmurro",
-    pages: 256,
-    isRead: true,
-    isFavorite: false
-  },
-  {
-    id: 4,
-    title: "A Revolução dos Bichos",
-    pages: 152,
-    isRead: true,
-    isFavorite: false
-  },
-  {
-    id: 5,
-    title: "O Pequeno Príncipe",
-    pages: 96,
-    isRead: true,
-    isFavorite: true
-  },
-  {
-    id: 6,
-    title: "Crime e Castigo",
-    pages: 671,
-    isRead: false,
-    isFavorite: false
-  },
-  {
-    id: 7,
-    title: "Harry Potter e a Pedra Filosofal",
-    pages: 223,
-    isRead: true,
-    isFavorite: true
-  },
-  {
-    id: 8,
-    title: "A Arte da Guerra",
-    pages: 112,
-    isRead: false,
-    isFavorite: false
-  }
-]
 
 function App() {
 
